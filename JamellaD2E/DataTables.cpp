73b068049340f3cb80e08e3d67fb44be21ec681d
Nedkali <nedkali@westnet.com.au>
2020-11-30 21:34:42 +1100

    Initial commit

---
+++ JamellaD2E/DataTables.cpp
@@ -0,0 +1,5 @@
+#include "JamellaD2E.h"
+char buffer[256];
+ItemInfo* SelInfo = 0;
+const char* CharClasses[5] = { "Amazon","Sorceress","Necromancer","Paladin","Barbarian" };
+const char* Difficulties[4] = { "Normal","Nightmare","Hell","Hell done" };
